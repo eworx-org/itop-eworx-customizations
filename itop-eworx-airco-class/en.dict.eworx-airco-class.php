8e6ea3d4012b3b8f3291272b77fbbd0efaeb7137
Ioannis Cherouvim <743305+cherouvim@users.noreply.github.com>
2024-12-02 08:52:04 +0200

    refactor: Remove dummy field and attempt to use default display EWX-669.

--- itop-eworx-airco-class/en.dict.eworx-airco-class.php
+++ itop-eworx-airco-class/en.dict.eworx-airco-class.php
@@ -8,9 +8,6 @@
 
 Dict::Add('EN US', 'English', 'English', array(
 	// Dictionary entries go here
-	'Class:AirCo+' => 'Air Condition',
-	'Class:AirCo/Attribute:technology' => 'Technology',
-	'Class:AirCo/Attribute:technology+' => 'Technology used for the display',
-	'Class:AirCo/Attribute:technology/Value:crt' => 'CRT',
+	'Class:AirCo' => 'Air Condition',
 ));
 ?>
