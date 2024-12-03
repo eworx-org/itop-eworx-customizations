cffa29f0ec41f94ba836b6c3db32c5b794b67a5c
Ioannis Cherouvim <743305+cherouvim@users.noreply.github.com>
2024-12-03 19:44:49 +0200

    feat: Peripheral adaptations EWX-669.

---
+++ itop-eworx-peripheral-class/en.dict.eworx-peripheral-class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:Peripheral/Attribute:ewx_lab_consumable' => 'Lab Consumable',
+	'Class:Peripheral/Attribute:ewx_lab_consumable+' => 'Whether this is to be consumed in the basement lab. If "no" then it\'s available to all.',
+	'Class:Peripheral/Attribute:ewx_lab_consumable/Value:no' => 'No',
+	'Class:Peripheral/Attribute:ewx_lab_consumable/Value:no+' => '',
+	'Class:Peripheral/Attribute:ewx_lab_consumable/Value:yes' => 'Yes',
+	'Class:Peripheral/Attribute:ewx_lab_consumable/Value:yes+' => '',
+	'Class:Peripheral/Attribute:ewx_category' => 'Category',
+	'Class:Peripheral/Attribute:ewx_category+' => '',
+	'Class:Peripheral/Attribute:ewx_subcategory' => 'Subcategory',
+	'Class:Peripheral/Attribute:ewx_subcategory+' => '',
+	'Class:Peripheral/Attribute:ewx_quantity' => 'Quantity',
+	'Class:Peripheral/Attribute:ewx_quantity+' => '',
+	'Class:Peripheral/Attribute:ewx_supplier' => 'Supplier',
+	'Class:Peripheral/Attribute:ewx_supplier+' => '',
+));
+?>
